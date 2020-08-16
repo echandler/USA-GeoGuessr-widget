092af7dcb020ece696c4a67942a7d7fb41beb741
echandler <chandleric@hotmail.com>
2020-08-16 00:58:52 -0700

    Additional fixes, bumped to version 6.

--- src.user.js
+++ src.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         GeoGuessr USA widget
 // @namespace    GeoGuessr scripts
-// @version      5.0
+// @version      6.0
 // @description  Interactive USA map for GeoGuessr.com.
-// @downloadURL  https://cdn.jsdelivr.net/gh/echandler/USA-GeoGuessr-widget/src.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/echandler/USA-GeoGuessr-widget/src.js
 // @include      https://www.geoguessr.com/*
 // @run-at       document-idle
 // @license      MIT
@@ -96,35 +96,28 @@ runAsClient(() => {
         .stateSelected {
             fill: ${stateColors.active};
         }
-        .countryOnload {
-            fill: transparent;
-            animation: countryInit 2s ease-in;
-            animation-delay: 3s;
-            animation-fill-mode: forwards;
-         }
 
         .stateOnload {
             opacity: 0;
             stroke-dashoffset: 1000;
             stroke-miterlimit: 10;
             stroke-dasharray: 1000;
-            animation: stateInit 3s ease-in;
-            animation-delay: 2s;
+            animation: stateOpacityOnload 1s ease-in, stateStrokeOnload 4s ease-in;
+            animation-delay: 3s;
             animation-fill-mode: forwards;
         }
 
-        @keyframes countryInit {
-            to {
-                fill:${stateColors.inactive};
-            }
-        }
-        @keyframes stateInit {
-            to {
-                stroke-dashoffset: 0;
+        @keyframes stateOpacityOnload {
+            100% {
                 opacity: 1;
             }
         }
 
+        @keyframes stateStrokeOnload {
+            100%{
+                stroke-dashoffset: 0;
+            }
+        }
         .blink{
           animation-name: example;
           animation-duration: ${aniDuration}ms;
@@ -147,7 +140,7 @@ runAsClient(() => {
     <!-
     <path xmlns="http://www.w3.org/2000/svg" id="frames" fill="none" stroke="#A9A9A9" stroke-width="2" d="M215 493v55l36 45M0 425h147l68 68h85l54 54v46"/>
     ->
-    <g xmlns="http://www.w3.org/2000/svg" class="country countryOnload">
+    <g xmlns="http://www.w3.org/2000/svg" class="country">
       <path class="state" id="AK" d="M161.1 453.7l-.3 85.4 1.6 1 3.1.2 1.5-1.1h2.6l.2 2.9 7 6.8.5 2.6 3.4-1.9.6-.2.3-3.1 1.5-1.6 1.1-.2 1.9-1.5 3.1 2.1.6 2.9 1.9 1.1 1.1 2.4 3.9 1.8 3.4 6 2.7 3.9 2.3 2.7 1.5 3.7 5 1.8 5.2 2.1 1 4.4.5 3.1-1 3.4-1.8 2.3-1.6-.8-1.5-3.1-2.7-1.5-1.8-1.1-.8.8 1.5 2.7.2 3.7-1.1.5-1.9-1.9-2.1-1.3.5 1.6 1.3 1.8-.8.8s-.8-.3-1.3-1c-.5-.6-2.1-3.4-2.1-3.4l-1-2.3s-.3 1.3-1 1c-.6-.3-1.3-1.5-1.3-1.5l1.8-1.9-1.5-1.5v-5h-.8l-.8 3.4-1.1.5-1-3.7-.6-3.7-.8-.5.3 5.7v1.1l-1.5-1.3-3.6-6-2.1-.5-.6-3.7-1.6-2.9-1.6-1.1v-2.3l2.1-1.3-.5-.3-2.6.6-3.4-2.4-2.6-2.9-4.8-2.6-4-2.6 1.3-3.2V542l-1.8 1.6-2.9 1.1-3.7-1.1-5.7-2.4h-5.5l-.6.5-6.5-3.9-2.1-.3-2.7-5.8-3.6.3-3.6 1.5.5 4.5 1.1-2.9 1 .3-1.5 4.4 3.2-2.7.6 1.6-3.9 4.4-1.3-.3-.5-1.9-1.3-.8-1.3 1.1-2.7-1.8-3.1 2.1-1.8 2.1-3.4 2.1-4.7-.2-.5-2.1 3.7-.6v-1.3l-2.3-.6 1-2.4 2.3-3.9v-1.8l.2-.8 4.4-2.3 1 1.3h2.7l-1.3-2.6-3.7-.3-5 2.7-2.4 3.4-1.8 2.6-1.1 2.3-4.2 1.5-3.1 2.6-.3 1.6 2.3 1 .8 2.1-2.7 3.2-6.5 4.2-7.8 4.2-2.1 1.1-5.3 1.1-5.3 2.3 1.8 1.3-1.5 1.5-.5 1.1-2.7-1-3.2.2-.8 2.3h-1l.3-2.4-3.6 1.3-2.9 1-3.4-1.3-2.9 1.9h-3.2l-2.1 1.3-1.6.8-2.1-.3-2.6-1.1-2.3.6-1 1-1.6-1.1v-1.9l3.1-1.3 6.3.6 4.4-1.6 2.1-2.1 2.9-.6 1.8-.8 2.7.2 1.6 1.3 1-.3 2.3-2.7 3.1-1 3.4-.6 1.3-.3.6.5h.8l1.3-3.7 4-1.5 1.9-3.7 2.3-4.5 1.6-1.5.3-2.6-1.6 1.3-3.4.6-.6-2.4-1.3-.3-1 1-.2 2.9-1.5-.2-1.5-5.8-1.3 1.3-1.1-.5-.3-1.9-4 .2-2.1 1.1-2.6-.3 1.5-1.5.5-2.6-.6-1.9 1.5-1 1.3-.2-.6-1.8v-4.4l-1-1-.8 1.5h-6.1l-1.5-1.3-.6-3.9-2.1-3.6v-1l2.1-.8.2-2.1 1.1-1.1-.8-.5-1.3.5-1.1-2.7 1-5 4.5-3.2 2.6-1.6 1.9-3.7 2.7-1.3 2.6 1.1.3 2.4 2.4-.3 3.2-2.4 1.6.6 1 .6h1.6l2.3-1.3.8-4.4s.3-2.9 1-3.4c.6-.5 1-1 1-1l-1.1-1.9-2.6.8-3.2.8-1.9-.5-3.6-1.8-5-.2-3.6-3.7.5-3.9.6-2.4-2.1-1.8-1.9-3.7.5-.8 6.8-.5h2.1l1 1h.6l-.2-1.6 3.9-.6 2.6.3 1.5 1.1-1.5 2.1-.5 1.5 2.7 1.6 5 1.8 1.8-1-2.3-4.4-1-3.2 1-.8-3.4-1.9-.5-1.1.5-1.6-.8-3.9-2.9-4.7-2.4-4.2 2.9-1.9h3.2l1.8.6 4.2-.2 3.7-3.6 1.1-3.1 3.7-2.4 1.6 1 2.7-.6 3.7-2.1 1.1-.2 1 .8 4.5-.2 2.7-3.1h1.1l3.6 2.4 1.9 2.1-.5 1.1.6 1.1 1.6-1.6 3.9.3.3 3.7 1.9 1.5 7.1.6 6.3 4.2 1.5-1 5.2 2.6 2.1-.6 1.9-.8 4.8 1.9zM46 482.6l2.1 5.3-.2 1-2.9-.3-1.8-4-1.8-1.5H39l-.2-2.6 1.8-2.4 1.1 2.4 1.5 1.5zm-2.6 33.5l3.7.8 3.7 1 .8 1-1.6 3.7-3.1-.2-3.4-3.6zM22.7 502l1.1 2.6 1.1 1.6-1.1.8-2.1-3.1V502zM9 575.1l3.4-2.3 3.4-1 2.6.3.5 1.6 1.9.5 1.9-1.9-.3-1.6 2.7-.6 2.9 2.6-1.1 1.8-4.4 1.1-2.7-.5-3.7-1.1-4.4 1.5-1.6.3zm48.9-4.5l1.6 1.9 2.1-1.6-1.5-1.3zm2.9 3l1.1-2.3 2.1.3-.8 1.9h-2.4zm23.6-1.9l1.5 1.8 1-1.1-.8-1.9zm8.8-12.5l1.1 5.8 2.9.8 5-2.9 4.4-2.6-1.6-2.4.5-2.4-2.1 1.3-2.9-.8 1.6-1.1 1.9.8 3.9-1.8.5-1.5-2.4-.8.8-1.9-2.7 1.9-4.7 3.6-4.8 2.9zm42.3-19.8l2.4-1.5-1-1.8-1.8 1z" style="">
         <title>Alaska</title>
       </path>
@@ -307,15 +300,12 @@ runAsClient(() => {
     setTimeout(function () {
         // Remove onload css animations.
 
-        let country = document.body.querySelector(".country");
-        country.classList.remove("countryOnload");
-
         let paths = document.body.querySelectorAll(".state");
         paths.forEach((path) => {
             // Initiate paths.
             path.classList.remove("stateOnload");
         });
-    }, 5000);
+    }, 10000);
 
     let paths = document.body.querySelector("#svg_usa").querySelectorAll("path");
     let prevState = _localStorage("get");
@@ -325,6 +315,7 @@ runAsClient(() => {
         if (path.getAttribute("id") == "frames") return;
 
         path.classList.add("stateOnload");
+        //  path.classList.add("stateFillOnload");
 
         path.addEventListener("mousedown", function () {
             path.addEventListener("mousemove", _mouseMove);
@@ -442,19 +433,6 @@ runAsClient(() => {
 
         google.maps.OverlayView.prototype._usaWidget.callbacks.push(function () {
             if (!showDebugPopups) return;
-
-            //  var x = new MutationObserver((e) => {
-            //      if (e[0].removedNodes) {
-            //          let msg = document.getElementById("tempMsg");
-            //          if (msg) {
-            //              msg.parentElement.removeChild(msg);
-            //          }
-            //          // console.log("removed",e, this._data);
-            //          x.disconnect();
-            //      }
-            //  });
-            //
-            //  x.observe(this.div, { childList: true, subtree: true });
 
             this.div.addEventListener("mouseover", (e) => {
                 let msg = document.getElementById("tempMsg");
